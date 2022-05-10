9b9a0820ae029eeff820a93bc13811672bd9710b
Kenta Suzuki <kenta.suzuki@rakus-partners.co.jp>
2022-05-10 13:42:11 +0900

    first commit

---
+++ src/test/java/com/example/GitSampleKSuzukiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleKSuzukiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
